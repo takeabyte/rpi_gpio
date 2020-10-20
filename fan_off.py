221991403a50e424e02fd40c9044d4ce47a13ddc
takeabyte <ttjunghardt@gmail.com>
2020-10-20 12:47:16 +0100

    update

---
+++ fan_off.py
@@ -0,0 +1,7 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(26, GPIO.OUT)  
+
+GPIO.output(26, GPIO.HIGH)
+GPIO.output(26, 0)
+GPIO.cleanup()
---
+++ fan_on.py
@@ -0,0 +1,6 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(26, GPIO.OUT)
+GPIO.output(26, GPIO.HIGH)
+GPIO.output(26, 1)
+GPIO.cleanup() //clean
