a4ec7867934fee4fdc29e3e5428474840efdd695
takeabyte <takeabyte@users.noreply.github.com>
2021-09-14 22:47:23 +0200

    fan off

--- fan_off.py
+++ fan_off.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-
 import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.OUT)
 #switch off GPO26
 GPIO.output(26, GPIO.HIGH)
-GPIO.cleanup() 
+GPIO.cleanup()
--- fan_on.py
+++ fan_on.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-
 import RPi.GPIO as GPIO
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.OUT)
 #switch on Relay with GPO26
